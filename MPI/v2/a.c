005ecfb57c730e0d720e41057eb9610ca6bc028a
Mariana <57501545+wari99@users.noreply.github.com>
2025-11-18 15:32:21 -0300

    fixed code
    Refactor file writing logic and remove unused function.

--- MPI/v2/a.c
+++ MPI/v2/a.c
@@ -1,23 +1,10 @@
+
 // mpicc a.c -o a 
 // mpirun -np 10 -oversubscribe ./a 12 uerjjjj
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-
-void escreve_no_arquivo(int rank, int valor, const char *nome_arquivo)
-{
-    FILE *arquivo = fopen(nome_arquivo, "a");  // Abre o arquivo em modo de append
-
-    if (arquivo == NULL) {
-        fprintf(stderr, "Erro ao abrir o arquivo\n");
-        return;
-    }
-
-    fprintf(arquivo, "Identificacao = %d valor %d\n", rank, valor);
-
-    fclose(arquivo);
-}
 
 int main(int argc, char **argv)
 {
@@ -28,33 +15,48 @@ int main(int argc, char **argv)
 
     int numTasks, rank, dest, source, count, tag = 1;
     int inmsg, outmsg;
+    int valor_inicial, i;
 
     MPI_Status Stat;
-    const char *nome_arquivo = argv[2];  // Nome do arquivo passado como argumento
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numTasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    int valor_inicial = atoi(argv[1]);  // O primeiro argumento é o valor inicial
-    for (int i = 0; i < numTasks; ++i) {
-        if (i == 0 && rank == 0) {
-            dest = rank + 1;
-            valor_inicial += rank;
-            outmsg = valor_inicial;
-            escreve_no_arquivo(rank, outmsg, nome_arquivo);
-            MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+    if (rank == 0){
+        valor_inicial = atoi(argv[1]); 
+        FILE *arquivo = fopen(argv[2], "w");  // Abre o arquivo em modo de append
+        if (arquivo == NULL) {
+             fprintf(stderr, "Erro ao abrir o arquivo\n");
+            return -1;
         }
-        else if (rank == i) {
+        dest = rank+1;
+        fprintf(arquivo, "Identificacao = %d valor %d\n", rank, valor_inicial);
+        MPI_Send(&valor_inicial, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+        for (i = 1; i < numTasks; ++i){
+            MPI_Recv(&inmsg, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &Stat);
+            fprintf(arquivo, "Identificacao = %d valor %d\n", i, inmsg);
+
+        }
+        fclose(arquivo);
+    }
+    else{
+        if (rank == numTasks-1){
             source = rank - 1;
-            dest = rank + 1;
             MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &Stat);
-            inmsg += rank;
-            outmsg = inmsg;
-            escreve_no_arquivo(rank, outmsg, nome_arquivo);
-            if (i != numTasks - 1)
-                MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+            inmsg = rank+ inmsg;
+             MPI_Send(&inmsg, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
         }
+        else{
+            dest = rank + 1;
+            source = rank - 1;
+            MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &Stat);
+            inmsg = rank+ inmsg;
+            MPI_Send(&inmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+            MPI_Send(&inmsg, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+
+        }
+        
     }
 
     MPI_Finalize();
